fa25804cd220bea01e01867b5101c647cb4add02
roosapi <roosapi@protonmail.com>
2026-01-22 13:37:19 +0100

    Add base for AddHorseForm

---
+++ src/components/HorseForms.jsx
@@ -0,0 +1,20 @@
+export const AddHorseForm = () => {
+    return (
+        <form className="input-form">
+            <label htmlFor="id">Horse ID</label>
+            <input type="number" name="id" />
+            <label htmlFor="name">Name</label>
+            <input type="string" name="name" />            
+            <label htmlFor="sex">Sex</label>
+            <input type="string" name="sex" />
+            <label htmlFor="breed">Breed</label>
+            <input type="string" name="breed" />
+            <div>
+                <button type="button" className="confirm-button">Save</button>
+                <span />
+                <button type="button" className="cancel-button">Cancel
+                </button>
+            </div>
+        </form>
+    );
+}
--- src/components/HorsesPage.jsx
+++ src/components/HorsesPage.jsx
@@ -1,10 +1,14 @@
 import { MOCK_HORSES } from "../horses/MockHorses";
 import HorseList from "./HorseList";
+import { AddHorseForm } from "./HorseForms";
 
 function HorsesPage() {
     return (
     <>
     <h1>Horses</h1>
+    <h2>Add Horse</h2>
+    <AddHorseForm />
+    <h1>All horses</h1>
     <HorseList horses={MOCK_HORSES}/>
     
     </>
