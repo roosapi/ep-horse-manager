77d2edf67ba7b5287e56b2fe45b60dce3253dbd8
roosapi <roosapi@protonmail.com>
2026-01-22 16:50:41 +0100

    Add scaffolding to having multiple pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
+
+
+import { useState } from 'react';
+import { createRoot } from "react-dom/client";
+
 import './index.css'; // import css
 
-import HorsesPage from './components/HorsesPage';
+import {AddHorsePage, HorsesPage} from './components/HorsesPage';
 
 
-import { createRoot } from "react-dom/client";
+
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <App />
 );
 
+// TODO: change state based on button click 
+// and show appropriate page
 function App() {
-  return (
-    <HorsesPage />
-  );
+  const [activePage,setActivePage] = useState();
+  let use_page;
+  if (activePage === "horses") {
+    use_page=<HorsesPage />;
+   } else if (activePage === "add-horse") {
+    use_page=<AddHorsePage />;
+   }
+
+   return (
+    <>
+    <div><button>Horses</button>
+    <button>Add New Horse</button></div>
+    {use_page}
+    </>
+   );
+    
 }
 
--- src/components/HorsesPage.jsx
+++ src/components/HorsesPage.jsx
@@ -2,17 +2,23 @@ import { MOCK_HORSES } from "../horses/MockHorses";
 import HorseList from "./HorseList";
 import { AddHorseForm } from "./HorseForms";
 
-function HorsesPage() {
+export const HorsesPage = () => {
     return (
     <>
     <h1>Horses</h1>
-    <h2>Add Horse</h2>
-    <AddHorseForm />
-    <h1>All horses</h1>
     <HorseList horses={MOCK_HORSES}/>
     
     </>
     )
 }
 
-export default HorsesPage;
+
+export const AddHorsePage = () => {
+    return (
+    <>
+
+    <h1>Add Horse</h1>
+    <AddHorseForm />s
+    </>
+    )
+}
