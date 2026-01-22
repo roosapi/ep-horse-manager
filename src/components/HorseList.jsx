a49e6d355be2744034f975454d1050bb0363e073
roosapi <roosapi@protonmail.com>
2026-01-22 13:23:27 +0100

    Add style to HorseList

--- src/components/HorseList.jsx
+++ src/components/HorseList.jsx
@@ -2,14 +2,28 @@ import PropTypes from 'prop-types';
 import { Horse } from '../horses/Horse';
 
 const HorseListItem = ({horse}) => {
-    return <tr><td>{horse.id}</td><td>{horse.name}</td></tr>
+    return (
+    <tr>
+        <td>{horse.id}</td>
+        <td>{horse.name}</td>
+        <td>{horse.gender}</td>
+        <td>{horse.breed}</td>
+    </tr>
+    );
 }
 
 const HorseList = ({horses}) => {
     const listRows = horses.map((horse)=> (
         <HorseListItem key={horse.id} horse={horse} />
     ))
-    return <table><tbody>{listRows}</tbody></table>;
+    return ( 
+    <table className="horse-table">
+        <tbody>
+            <tr><th>ID</th><th>Name</th><th>Sex</th><th>Breed</th></tr>
+            {listRows}
+        </tbody>
+    </table>
+    );
 }
 
 HorseList.PropTypes = {
