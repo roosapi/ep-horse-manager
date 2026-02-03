fc467c4e1193cd55d26c0ecb0166df84a4ffca3b
roosapi <roosapi@protonmail.com>
2026-02-03 12:22:45 +0100

    Use Tanstack Table for the horses table

--- src/routes/horses/-components/HorseList.jsx
+++ src/routes/horses/-components/HorseList.jsx
@@ -1,84 +1,114 @@
-import { useEffect, useState, Fragment } from 'react';
+import { useEffect, useState, Fragment, useMemo } from 'react';
+import { createColumnHelper, useReactTable, getCoreRowModel, flexRender } from '@tanstack/react-table';
 
 import * as Constants from '../../../utilities/constants'
 
-const SkillListItem = ({skillsArray}) => {
-    const skillSum = skillsArray.reduce((acc,current) => acc + current);
-    return (
-        <>
-        <td>{skillsArray[0]}</td>
-        <td>{skillsArray[1]}</td>
-        <td>{skillsArray[2]}</td>
-        <td>{skillsArray[3]}</td>
-        <td>{skillsArray[4]}</td>
-        <td>{skillSum}</td>
-        </>
-    )
+const getSkillColumns = (discp,columnHelper) => {
+        const abbrv = Constants.disciplineMap.get(discp);
+        const colNames = [1,2,3,4,5].map((idx) => (abbrv+`_skill${idx}`));
+        const baseCols = colNames.map((cName) => (
+                    columnHelper.accessor(cName, {
+                        header:'',
+                    })
+        ));
+        const sumCol = columnHelper.display({
+            id:abbrv+'_sum',
+            header:'∑',
+            cell: (info) => colNames.reduce(
+                (total,cellName)=>info.row.original[cellName] + total,
+                0)
+        });
+        return baseCols.concat(sumCol);
+    };
 
-}
-
-const HorseListItem = ({horse}) => {
-    const skills = new Map([...Constants.disciplineMap.values()].map(discp => {
-        return [discp,[]]
-    }));
-
-    // Extract and group all skills for each discipline
-    // NOTE! This assumes in horse keys the skills are always ordered skill1-skill5,
-    // which should be the case but might not?
-    Object.keys(horse).forEach((key) => {
-        if (key.includes('_skill')) {
-            let discp = key.slice(0,2);
-            skills.set(discp,skills.get(discp).concat([horse[key]]))
-        }
-    }); 
-    const horseSkillsItems = [...skills.keys()].map((skill_key)=> (
-        <SkillListItem key={skill_key} skillsArray={skills.get(skill_key)} />
-    ));
-
-    return (
-    <tr>
-        <td>{horse.id}</td>
-        <td>{horse.name}</td>
-        <td>{horse.sex}</td>
-        <td>{horse.breed}</td>
-        <td>{horse.type}</td>
-        <td>{horse.height}</td>
-        <td>{horse.born}</td>
-        {horseSkillsItems}
-    </tr>
-    );
+const getHorseColumns = () => {
+    const columnHelper = createColumnHelper();
+    const horseColumns = [
+        columnHelper.group({
+            header:'Basic Information',
+            columns: [
+                columnHelper.accessor('id', {
+                    header: () => 'ID'
+                }),                
+                columnHelper.accessor('name', {
+                    header: () => 'Name'
+                }),
+                columnHelper.accessor('sex', {
+                    header: () => 'Sex'
+                }),
+                columnHelper.accessor('breed', {
+                    header: () => 'Breed'
+                }),
+                columnHelper.accessor('type', {
+                    header: () => 'Type'
+                }),
+                columnHelper.accessor('height', {
+                    header: () => 'H (cm)'
+                }),
+                columnHelper.accessor('born', {
+                    header: () => 'Born'
+                })
+            ]
+        }),
+        columnHelper.group({
+            header:'Skills',
+            columns: [...Constants.disciplineMap.keys()].map((discp) => (
+                columnHelper.group({
+                    header:discp,
+                    columns: getSkillColumns(discp,columnHelper)
+                })
+            ))
+        }),
+    ];
+    return horseColumns;
 }
 
 const HorseList = () => {
     const [horseData, setHorseData] = useState([]);
+    const horseColumns = useMemo(getHorseColumns,[]);
 
     useEffect(() => {
         window.databaseAPI.getHorses().then(setHorseData);
     },[]);
-    
-    const horseListItems = horseData.map((horse)=> (
-        <HorseListItem key={horse.id} horse={horse} />
-    ));
+
+    const horseTable = useReactTable({
+        data: horseData,
+        columns: horseColumns,
+        getCoreRowModel: getCoreRowModel(),
+    });
+
 
     return ( 
     <table className="horse-table">
+        <thead>
+            {
+                horseTable.getHeaderGroups().map(headerGroup => (
+                    
+                    <tr key={headerGroup.id}>
+                        {headerGroup.headers.map(header => (
+                                <th key={header.id} colSpan={header.colSpan}>
+                                {header.isPlaceholder
+                                ? null
+                                : flexRender(
+                                    header.column.columnDef.header,
+                                    header.getContext(),
+                                )}
+                            </th>
+                        ))}
+                    </tr>
+
+            ))}
+        </thead>
         <tbody>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Sex</th>
-                <th>Breed</th>
-                <th>Type</th>
-                <th>H (cm)</th>
-                <th>Born</th>
-                {[...Constants.disciplineMap.keys()].map((discp) => (
-                    <Fragment key={Constants.disciplineMap.get(discp)} >
-                    <th colSpan="5">{discp}</th>
-                    <th >∑</th>
-                    </Fragment>
-                ))}
-                </tr>
-            {horseListItems}
+            {horseTable.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
     </table>
     );
--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,19 +1,4 @@
 
-// TODO convert to Map
- const disciplines = {
-    'Dressage':'DR',
-    'Show Jumping':'SJ',
-    'Cross Country':'XC',
-    'Horse Driving':'HD', 
-    'Gaited Competitions':'GC',
-    'Reining':'RE', 
-    'Endurance':'EN', 
-    'Flat Racing':'FR',
-    'Baroque Riding':'BR',
-    'Trail':'TR', 
-    'Horse Logging':'HL'
-}
-
 const disciplineMap = new Map([
   ['Dressage', 'DR'],
   ['Show Jumping', 'SJ'],
